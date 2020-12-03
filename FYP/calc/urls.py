50ac15e609bd90b213066436e2187c2f5ff73060
Hedeiwy <mhedeiwym@gmail.com>
2020-12-03 23:56:25 +0800

    new things

--- FYP/calc/urls.py
+++ FYP/calc/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # views.home is a function in the views class thats why we imported it from above
     # now if someone wants to go home this is what they will see
     # this is not enough you also have to go to the projects ulr and also let it know about this app as the projects url is the core mapping to every app
-    path('', views.home, name='home') 
+    path('', views.home, name='home'), 
+    path('add', views.add, name='add')
 ]
--- FYP/calc/views.py
+++ FYP/calc/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
 def home(request):
     x = 1 + 19
-    return render(request, 'home.html',{'name': 'Hedeiwy', 'age': x}) # this is what the home function will return to the urls
+    # this is what the home function will return to the urls
+    return render(request, 'home.html', {'name': 'Hedeiwy', 'age': x})
+
+
+def add(request):
+
+    a = int(request.GET['num'])
+    b= int(request.GET['num2'])
+    res= a + b
+
+    return render(request, 'result.html', {'addition': res})
